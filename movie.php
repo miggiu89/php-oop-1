1db26e1719c2a863b33929593b8833fc47c40441
Matteo <matteomigliori280489@gmail.com>
2021-05-11 08:51:34 +0200

    push

--- movie.php
+++ movie.php
@@ -6,12 +6,24 @@
         public $trama;
         public $anno;
         public $cast;
-        public $sconto = 0;
+        private $sconto = 0;
+        public $eta_user;
 
-        public function __construct($titolo,$trama,$anno) {
+        public function __construct($titolo,$trama,$anno,$eta_user) {
             $this->titolo = $titolo;
             $this->trama = $trama;
              $this->anno = $anno;
+             $this->eta_user =  $eta_user;
+        }
+
+        public function setSconto($eta_user){
+            if($eta_user < 18 ){
+                $this -> sconto = 30;
+            }
+        }
+
+        public function getSconto(){
+            return $this-> sconto;
         }
 
     }
@@ -28,15 +40,41 @@
     //prova costrutto
     
 
-    $filmA = new Movie ('Non è un paese per vecchi', 'secondo Lorem ipsum',1986);
-    $filmB = new Movie ('Noi ragazzi dello zoo di Berlino', 'secondo Lorem ipsum',1986);
-    $filmC = new Movie ('Split','terzo lorem ipsum',2016);
-    //
-     echo 'Titolo: ' .$filmA->titolo .'<br>' .'Trama: ' .$filmA->trama .'<br>' .'Anno di uscita: '.$filmA->anno;
+    $filmA = new Movie ('Non è un paese per vecchi', 'secondo Lorem ipsum', 2007, 19);
+    $filmB = new Movie ('Noi ragazzi dello zoo di Berlino', 'secondo Lorem ipsum',1986, 14);
+    $filmC = new Movie ('Split','terzo lorem ipsum',2016, 22);
+    
+     echo 'Titolo: ' .$filmA->titolo .'<br>' .'Trama: ' .$filmA->trama .'<br>' .'Anno di uscita: '.$filmA->anno .'<br>'.'Sconto applicato: ' .$filmA->setSconto($filmA->eta_user);
      echo '<br>';
-     echo 'Titolo: ' .$filmB->titolo .'<br>' .'Trama: ' .$filmB->trama .'<br>' .'Anno di uscita: '.$filmB->anno;
+     echo 'Titolo: ' .$filmB->titolo .'<br>' .'Trama: ' .$filmB->trama .'<br>' .'Anno di uscita: '.$filmB->anno .'<br>'.'Sconto applicato: ' .$filmB->setSconto($filmB->eta_user);
      echo '<br>';
-     echo 'Titolo: ' .$filmC->titolo .'<br>' .'Trama: ' .$filmC->trama .'<br>' .'Anno di uscita: '.$filmC->anno;
+     echo 'Titolo: ' .$filmC->titolo .'<br>' .'Trama: ' .$filmC->trama .'<br>' .'Anno di uscita: '.$filmC->anno .'<br>'.'Sconto applicato: ' .$filmC->setSconto($filmC->eta_user);
+
+
+     $Films = [$filmA,$filmB,$filmC]
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Movies</h1>
+        <div class="movie">
+            <?php foreach ($Films as $film){?>
+                <h3><?php $film->titolo ?></h3>
+                <p>Trama:<?php $film->trama ?></p>
+                <p>Anno uscita:<?php $film->trama ?></p>
+           <?php } ?>
+        
+        </div>
+    
+</body>
+</html>
